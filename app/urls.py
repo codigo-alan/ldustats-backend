e3be62dc2d20f1c316020f6c2db4344ca75d6cc4
Alan <alanfmarcos@gmail.com>
2023-06-18 22:34:37 +0200

    New endpoint not works

--- app/urls.py
+++ app/urls.py
@@ -5,9 +5,8 @@ from app import views
 
 router = routers.DefaultRouter()
 router.register(r'players', views.PlayerView, 'players') 
-router.register(r'uploadcsv', views.upload_csv, 'uploadcsv') 
+#router.register(r'uploadcsv', views.upload_csv, 'uploadcsv') 
 
 urlpatterns = [ 
-    path("api/v1/", include(router.urls)),
-    path("api/v1/uploadcsv", views.upload_csv) 
+    path("api/v1/", include(router.urls))
 ]
