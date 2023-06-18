6d627685160e55d2876eefca5a733d10a20961e5
Alan <alanfmarcos@gmail.com>
2023-06-18 22:27:40 +0200

    endpoint not found

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ from app import views
 
 router = routers.DefaultRouter()
 router.register(r'players', views.PlayerView, 'players') 
+router.register(r'uploadcsv', views.upload_csv, 'uploadcsv') 
 
 urlpatterns = [ 
     path("api/v1/", include(router.urls)),
