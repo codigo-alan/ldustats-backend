0f647d1f1a3e4030afc859bf585691b49ec370ce
Alan <alanfmarcos@gmail.com>
2023-06-27 18:31:51 +0200

    Modified the return in models

--- app/models.py
+++ app/models.py
@@ -24,21 +24,21 @@ class Player(models.Model):
     position = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name
+        return self.id
     
 class File(models.Model):
     id = models.CharField(max_length=20, primary_key=True)
     date = models.DateField()
 
     def __str__(self):
-        return f"{self.id}, {self.date}"
+        return f"{self.id}"
     
 class Session(models.Model):
     name = models.CharField(max_length=200)
     date = models.DateField()
     distance = models.DecimalField(max_digits=4, decimal_places=2)
-    idPlayer = models.ForeignKey(Player, on_delete=models.CASCADE, null=True, to_field='id')
-    idFile = models.ForeignKey(File, on_delete=models.CASCADE, null=True, to_field='id')
+    idPlayer = models.ForeignKey(Player, on_delete=models.CASCADE, null=True)
+    idFile = models.ForeignKey(File, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"{self.name}, {self.date}, {self.distance}, {self.idPlayer}, {self.idFile}"
