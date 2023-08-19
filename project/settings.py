9538b6874824ac48c059dad631e949a9848576ad
Alan <alanfmarcos@gmail.com>
2023-08-19 15:51:07 +0200

    Change setting to add postgre

--- project/settings.py
+++ project/settings.py
@@ -78,10 +78,20 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'ldu',
+
+        'USER': 'ldu',
+
+        'PASSWORD': 'ldu',
+
+        'HOST': '127.0.0.1',
+
+        'PORT': '5432',
     }
+
 }
 
 
