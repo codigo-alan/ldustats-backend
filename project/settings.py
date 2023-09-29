c947e3d302815a9d1bf47e745aa4baa5a83a536f
Alan <alanfmarcos@gmail.com>
2023-09-30 00:03:39 +0200

    Add cors auth to ldustats netlify

--- project/settings.py
+++ project/settings.py
@@ -148,6 +148,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Cors authored to call this backend
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
+    'https://ldustats.netlify.app',
 ]
 
 # Log console while debug Runserver
